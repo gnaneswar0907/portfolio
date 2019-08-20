35dbc06a6e9418846a31a5f097add5e04f109daf
gnaneswar0907 <asish.gnan@gmail.com>
2019-08-19 22:37:13 -0500

    Changed Email API

--- emailAPI/index.js
+++ emailAPI/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const nodemailer = require("nodemailer");
 const cors = require("cors");
+const sgMail = require("@sendgrid/mail");
 
 const app = express();
 
-const port = 4444;
+const port = process.env.PORT;
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,32 +14,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 app.listen(port, () => {
-  console.log("We are live on port 4444");
+  console.log("We are live");
 });
 
 app.post("/api/sendMail", (req, res) => {
   const data = req.body;
-  const smtpTransport = nodemailer.createTransport({
-    service: "gmail",
-    port: 465,
-    auth: {
-      user: "asish.gnan@gmail.com",
-      pass: "ashishGNANESWAR1!"
-    }
-  });
-  var mailOptions = {
-    from: data.email,
-    to: "asish.gnan2@gmail.com",
-    subject: data.subject,
-    html: `<h2>${data.name}</h2>
-            <div>${data.message}</div>`
-  };
-  smtpTransport.sendMail(mailOptions, (error, response) => {
-    if (error) {
-      res.send(error);
-    } else {
-      res.send("Success");
-    }
-    smtpTransport.close();
-  });
+  sgMail
+    .send({
+      to: "asish.gnan2@gmail.com",
+      from: data.email,
+      subject: data.subject,
+      text: data.message
+    })
+    .then(() => res.send("Success"))
+    .catch(error => res.send(error));
 });
